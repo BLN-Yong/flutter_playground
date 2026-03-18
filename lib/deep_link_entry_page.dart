ecee776a291e8de85575738cf21c79f181f71bba
Kerkwit Supawattanakul <kerkwit@balerion.co.th>
2026-03-18 14:01:49 +0700

    update splash screen

--- lib/deep_link_entry_page.dart
+++ lib/deep_link_entry_page.dart
@@ -1,7 +1,13 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:provider/provider.dart';
 import 'package:quick_deep_link/deep_link_provider.dart' hide Provider;
 import 'package:webview_flutter/webview_flutter.dart';
+
+const _baseUrl =
+    'https://bln-rmos-super-app-web-dev-659453389471.asia-southeast1.run.app/';
 
 class DeepLinkEntryPage extends StatefulWidget {
   const DeepLinkEntryPage({super.key});
@@ -11,49 +17,61 @@ class DeepLinkEntryPage extends StatefulWidget {
 }
 
 class _DeepLinkEntryPageState extends State<DeepLinkEntryPage> {
+  late final WebViewController _webViewController;
+  StreamSubscription<DeepLinkData>? _deepLinkSubscription;
+  DeepLinkData? _latestData;
+
+  @override
+  void initState() {
+    super.initState();
+    FlutterNativeSplash.remove();
+    _webViewController = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse(_baseUrl));
+
+    final deepLinkProvider = Provider.of<DeepLinkProvider>(
+      context,
+      listen: false,
+    );
+    _deepLinkSubscription = deepLinkProvider.state.listen((data) {
+      _loadDeepLinkUrl(data);
+      setState(() => _latestData = data);
+    });
+  }
+
+  @override
+  void dispose() {
+    _deepLinkSubscription?.cancel();
+    super.dispose();
+  }
+
+  void _loadDeepLinkUrl(DeepLinkData data) {
+    final segments = [
+      if (data.page.isNotEmpty) data.page,
+      ...data.params,
+    ].join('/');
+
+    final url = '$_baseUrl$segments';
+    _webViewController.loadRequest(Uri.parse(url));
+  }
+
   @override
   Widget build(BuildContext context) {
-    DeepLinkProvider deepLinkStream = Provider.of<DeepLinkProvider>(context);
-    return StreamBuilder<DeepLinkData>(
-      stream: deepLinkStream.state,
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          return Center(
-            child: Text(
-              "No deep link found",
-              style: TextStyle(color: Colors.black),
-            ),
-          );
-        } else {
-          return Padding(
-            padding: const EdgeInsets.only(top: 30),
-            child: Center(
-              child: DefaultTextStyle(
+    return Padding(
+      padding: const EdgeInsets.only(top: 30),
+      child: _latestData != null
+          ? Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(child: WebViewWidget(controller: _webViewController)),
+              ],
+            )
+          : const Center(
+              child: Text(
+                'No deep link found',
                 style: TextStyle(color: Colors.black),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(snapshot.data!.page),
-                    ...snapshot.data!.params.map((params) => Text(params)),
-                    // if (snapshot.data!.params.isNotEmpty)
-                    //   Expanded(
-                    //     child: WebViewWidget(
-                    //       controller: WebViewController()
-                    //         ..loadRequest(
-                    //           Uri.parse(
-                    //             "https://www.airbnb.com/${snapshot.data!.page}/${snapshot.data!.params.join('/')}",
-                    //           ),
-                    //         ),
-                    //     ),
-                    //   ),
-                  ],
-                ),
               ),
             ),
-          );
-        }
-      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:provider/provider.dart';
 import 'package:quick_deep_link/deep_link_provider.dart' hide Provider;
 import 'package:quick_deep_link/deep_link_entry_page.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
 
@@ -15,6 +18,7 @@ class MyApp extends StatelessWidget {
     DeepLinkProvider bloc = DeepLinkProvider();
     return MaterialApp(
       title: 'Deep link poc',
+      theme: ThemeData(colorSchemeSeed: Colors.white),
       home: Scaffold(
         body: Provider<DeepLinkProvider>(
           create: (context) => bloc,
